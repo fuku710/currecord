20f78b47b5bfabf3abccc76d19e363f0c566dd27
Fukuda Naoto <never9612@gmail.com>
2019-05-06 00:22:27 +0900

    Add delete and edit

--- records/urls.py
+++ records/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import RecordList, RecordDetail, RecordCreate
+from .views import RecordList, RecordDetail, RecordCreate, RecordUpdate, RecordDelete
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='records/index.html')),
-    path('records/', RecordList.as_view()),
-    path('records/new/', RecordCreate.as_view()),
-    path('records/<pk>/', RecordDetail.as_view()),
+    path('', TemplateView.as_view(
+        template_name='records/index.html'), name='records-top'),
+    path('records/', RecordList.as_view(), name='records-list'),
+    path('records/new/', RecordCreate.as_view(), name='records-new'),
+    path('records/<pk>/', RecordDetail.as_view(), name='records-detail'),
+    path('records/<pk>/edit/', RecordUpdate.as_view(), name='records-edit'),
+    path('records/<pk>/delete/', RecordDelete.as_view(), name='records-delete'),
 ]
--- records/views.py
+++ records/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView,  UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Record
 from .forms import RecordForm
 
@@ -15,3 +16,13 @@ class RecordDetail(DetailView):
 class RecordCreate(CreateView):
     model = Record
     form_class = RecordForm
+
+
+class RecordUpdate(UpdateView):
+    model = Record
+    form_class = RecordForm
+
+
+class RecordDelete(DeleteView):
+    model = Record
+    success_url = reverse_lazy('records-list')
