59472c7cfb872c049d18750d24aaf7adddf80379
Fukuda Naoto <never9612@gmail.com>
2019-05-06 20:35:17 +0900

    Set record form year

--- records/forms.py
+++ records/forms.py
@@ -1,11 +1,16 @@
 from django import forms
+from django.utils import timezone
 from .models import Record
+
+year = timezone.now().year
+
+YEARS = list(range(year - 5, year + 1))
 
 
 class RecordForm(forms.ModelForm):
     class Meta:
         model = Record
-        fields = ['name', 'date', 'note','image']
+        fields = ['name', 'date', 'note', 'image']
         widgets = {
-            'date': forms.SelectDateWidget()
+            'date': forms.SelectDateWidget(years=YEARS)
         }
