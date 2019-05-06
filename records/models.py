9b1e78e7232ca433752ceea4e8834d5ae58139f0
Fukuda Naoto <never9612@gmail.com>
2019-05-06 13:50:20 +0900

    Allow blank for note field

--- records/models.py
+++ records/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Record(models.Model):
     name = models.CharField(verbose_name='名前', max_length=30)
-    note = models.TextField(verbose_name='メモ', max_length=400, null=True)
+    note = models.TextField(
+        verbose_name='メモ', max_length=400, null=True, blank=True)
     date = models.DateField(verbose_name='日付')
 
     def get_absolute_url(self):
